f4fbe7428cebb1384c9797bc4d28e0e537ab7ed7
bth <gh@hautogdoad.com>
2024-09-26 20:33:16 -0700

    get working with mailgun api: expects 'api' as username
    so req.SetBasicAuth() was what i wanted, didn't understand that the curl
    invocation using --user 'api:key' really meant api was username

--- send.go
+++ send.go
@@ -2,39 +2,56 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 )
-
-type EmailForm struct {
-	From    string
-	To      string
-	Subject string
-	Text    string
-}
 
 func SendEmail(content string) error {
 	key := os.Getenv("MAILGUN_TEST_API_KEY")
 	endpoint := "https://api.mailgun.net/v3/sandbox1e7a4321500241bc88fbd6fb1ad7d544.mailgun.org/messages"
 
-	var e EmailForm
-	e.From = "bth  <mailgun@sandbox1e7a4321500241bc88fbd6fb1ad7d544.mailgun.org>"
-	e.To = "test@hautogdoad.com"
-	e.Subject = "Foo go test"
-	e.Text = "go test yeah"
+	From := "bth  <mailgun@sandbox1e7a4321500241bc88fbd6fb1ad7d544.mailgun.org>"
+	To := "test@hautogdoad.com"
+	Subject := "Foo go test"
+	Text := "go test yeah"
 
-	b, err := json.Marshal(e)
+	data := &bytes.Buffer{}
+	writer := multipart.NewWriter(data)
+	htmlFw, _ := writer.CreateFormField("html")
+	_, err := io.Copy(htmlFw, strings.NewReader(Text))
 	if err != nil {
-		return err
+		panic(err)
 	}
-	j := bytes.NewReader(b)
+	fromFw, _ := writer.CreateFormField("from")
+	_, err = io.Copy(fromFw, strings.NewReader(From))
+	if err != nil {
+		panic(err)
+	}
+	toFw, _ := writer.CreateFormField("to")
+	_, err = io.Copy(toFw, strings.NewReader(To))
+	if err != nil {
+		panic(err)
+	}
+	subjectFw, _ := writer.CreateFormField("subject")
+	_, err = io.Copy(subjectFw, strings.NewReader(Subject))
+	if err != nil {
+		panic(err)
+	}
+	writer.Close()
 
-	req, _ := http.NewRequest("POST", endpoint, j)
-	req.Header.Add("Authorization", "Bearer "+key)
-	req.Header.Add("Content-Type", "application/json")
+	payload := bytes.NewReader(data.Bytes())
+	req, err := http.NewRequest("POST", endpoint, payload)
+	if err != nil {
+		panic(err)
+	}
+
+	req.SetBasicAuth("api", key)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
 
 	res, err := http.DefaultClient.Do(req)
 	defer res.Body.Close()
