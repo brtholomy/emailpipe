e547180b52943faf03cf998d95d8af91317161a7
bth <gh@hautogdoad.com>
2024-09-28 16:20:23 -0700

    implement draft and prod flow: but the last stage isnt working yet

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 var source = flag.String("source", "./sample.xml", "path to source .xml file")
 var slug = flag.String("slug", "why-fractals", "slug of the post to publish")
 var status = flag.String("status", "draft", "status can be 'draft' or 'about_to_send'")
+var email_id = flag.String("email_id", "", "id of draft email previously created")
 
 func main() {
 	flag.Parse()
@@ -17,16 +18,14 @@ func main() {
 		panic(err)
 	}
 
+	i.Title = "foo bar!"
 	fmt.Println(i.Title)
 	fmt.Println(i.Description)
 
-	body, err := SendEmail(&i.Content, &i.Title, status)
+	body, err := SendEmail(&i.Content, &i.Title, status, email_id)
 	if err != nil {
 		panic(err)
 	}
-	// TODO: loop every time like this:
-	// 1. send draft, save id
-	// 2. pause and ask for confirmation to send to all
-	// 3. use the return id to send
+
 	fmt.Println(string(body))
 }
--- send.go
+++ send.go
@@ -7,12 +7,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 // foo@hautogdoad.com:
 var test_subscriber string = "b7c993fc-1baf-4ba7-81f5-e5cb096dcfa0"
 var tbartholomy string = "t@bartholomy.ooo"
+var baseurl string = "https://api.buttondown.email/v1/emails"
 
 type EmailPayload struct {
 	// pointers to allow them to be "optional": nil value will json.Marshal
@@ -28,15 +30,26 @@ type ResponsePayload struct {
 	Status string `json:"status"`
 }
 
-func SendEmail(content *string, subject *string, status *string) ([]byte, error) {
-	// TODO: fill in recepients every time
+// Loops like this:
+// 1. send draft, save id
+// 2. pause and ask for confirmation to send to all
+// 3. use the return id to send
+// also possible to feed in the email_id directly, if status="about_to_send"
+func SendEmail(content *string, subject *string, status *string, email_id *string) ([]byte, error) {
 	payload := EmailPayload{subject, content, status, nil, nil}
-	endpoint := "https://api.buttondown.email/v1/emails"
+
+	var endpoint string
+	// skip directly to prod if draft aleady exists:
 	if *status == "about_to_send" {
+		if *email_id == "" {
+			return nil, errors.New("sending to prod requires an email_id of a draft")
+		}
+		endpoint, _ = url.JoinPath(baseurl, *email_id)
 		return SendPayload(payload, endpoint)
 	}
 
 	// create draft
+	endpoint = baseurl
 	res, err := SendPayload(payload, endpoint)
 	if err != nil {
 		return nil, err
@@ -46,15 +59,36 @@ func SendEmail(content *string, subject *string, status *string) ([]byte, error)
 		return nil, err
 	}
 	if resp.Status != "draft" {
-		return res, errors.New("return status is not draft, cancelling send")
+		return nil, errors.New("return status is not draft, cancelling send")
 	}
 
 	// send draft
-	fmt.Println("email id: ", resp.Id)
-	endpoint = fmt.Sprintf("https://api.buttondown.email/v1/emails/%s/send-draft", resp.Id)
+	fmt.Println("email_id:", resp.Id)
+	endpoint, _ = url.JoinPath(baseurl, resp.Id, "send-draft")
 	payload.Recipients = []string{tbartholomy}
 	payload.Subscribers = []string{test_subscriber}
-	return SendPayload(payload, endpoint)
+	res, err = SendPayload(payload, endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	// continue to prod
+	fmt.Println("sent draft. continue to prod? Y/n:")
+	var Answer string
+	_, err = fmt.Scanln(&Answer)
+	if err != nil {
+		return nil, err
+	}
+	if Answer == "Y" {
+		// NOTE: difference is no /send-draft at the end, and status="about_to_send"
+		// TODO: this isn't working. why? keep getting "method not allowed"
+		endpoint, _ = url.JoinPath(baseurl, resp.Id)
+		stat := "about_to_send"
+		payload = EmailPayload{nil, nil, &stat, nil, nil}
+		return SendPayload(payload, endpoint)
+	}
+	fmt.Println("quitting")
+	return res, nil
 }
 
 func SendPayload(payload EmailPayload, endpoint string) ([]byte, error) {
